a7f3bdc828362372a58d8e8d134bee2a0d3ff98a
Almas <muratov422@bk.ru>
2026-03-15 14:28:10 +0500

    Price model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-from app.api import router as api_router
+from app.api.routes import router as api_router
+from app.db.base import Base
+from app.db.session import engine
+import app.models
 
 app = FastAPI(
     title="Test Task API",
 )
+
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+
 
 app.include_router(api_router)
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.db.base import Base
+
+from app.models.price import Price
