94d0a40dc42b3adef4c7a1e5ef83c77953d7a3f9
Almas <muratov422@bk.ru>
2026-03-15 13:35:48 +0500

    DB setup

--- db/session.py
+++ db/session.py
@@ -1,5 +1,20 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from app.config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+)
 
 
-class Base(DeclarativeBase):
-    pass
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
