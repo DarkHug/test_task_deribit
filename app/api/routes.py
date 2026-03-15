280b64e3bed0b513d2de6cbfe1ddc678ddec2fe1
Almas <muratov422@bk.ru>
2026-03-15 14:44:47 +0500

    Price Routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
 
-router = APIRouter(
-    prefix="/api",
-    tags=["api"],
-)
+from app.db.session import get_db
+from app.services import price_service
+
+router = APIRouter(prefix="/prices", tags=["prices"])
+DBSession = Annotated[Session, Depends(get_db)]
 
 
-@router.get("/hello")
-async def hello():
-    return {"message": "Hello World"}
+@router.get("/")
+def get_prices(ticker: str, db: DBSession):
+    return price_service.get_all_prices(db=db, ticker=ticker)
+
+
+@router.get("/latest")
+def get_latest_price(ticker: str, db: DBSession):
+    return price_service.get_last_price(db=db, ticker=ticker)
+
+
+@router.get("/by-date")
+def get_prices_by_date(
+        ticker: str,
+        timestamp: int,
+        db: DBSession,
+):
+    return price_service.get_prices_by_date(
+        db=db,
+        ticker=ticker,
+        timestamp=timestamp,
+    )
